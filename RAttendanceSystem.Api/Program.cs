502f94e470e2b8b70c8311fe4a3f821cc1d8eaf6
ngcsontrh <trhsonngoc12@gmail.com>
2025-09-25 09:47:33 +0700

    feat: add oidc validation

--- RAttendanceSystem.Api/Program.cs
+++ RAttendanceSystem.Api/Program.cs
@@ -1,13 +1,38 @@
+using Microsoft.IdentityModel.Tokens;
 using RAttendanceSystem.Api.Endpoints;
 using RAttendanceSystem.Application;
 using RAttendanceSystem.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddInfrastructureServices();
 builder.Services.AddApplicationServices();
-
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        var jwksUri = $"{builder.Configuration["Keycloak:Domain"]}/realms/{builder.Configuration["Keycloak:Realm"]}/protocol/openid-connect/certs";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            RoleClaimType = "roles",
+            NameClaimType = "preferred_username",
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuers = builder.Configuration.GetSection("Authentication:Authorities").Get<string[]>(),
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKeyResolver = (token, securityToken, kid, parameters) =>
+            {
+                using var client = new HttpClient();
+                var response = client.GetStringAsync(jwksUri).Result;
+                var keys = new JsonWebKeySet(response);
+                return keys.Keys.Where(k => k.Kid == kid);
+            }
+        };
+        options.RequireHttpsMetadata = false;
+    });
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
@@ -18,9 +43,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapEmployeeEndpoints();
 
 app.Run();
